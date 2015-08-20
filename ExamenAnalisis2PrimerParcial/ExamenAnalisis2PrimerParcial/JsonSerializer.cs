843559bc63af3fa8d0261d3658f6d1e8556b76a2
Slyther <joseph.a.griff_07@hotmail.com>
2015-08-20 11:35:22 -0600

    Brainstorming ideas.

--- ExamenAnalisis2PrimerParcial/ExamenAnalisis2PrimerParcial/JsonSerializer.cs
+++ ExamenAnalisis2PrimerParcial/ExamenAnalisis2PrimerParcial/JsonSerializer.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace ExamenAnalisis2PrimerParcial
 {
     public class JsonSerializer
     {
+        private readonly List<Type> _acceptedDataTypes = new List<Type>
+        {
+            typeof(int),
+            typeof(float),
+            typeof(String),
+            typeof(DateTime)
+        };
+
         public string Serialize(Object classobject)
         {
-            return "{}";
+            string serializedObject = "{\n";
+            var objectProperties = classobject.GetType().GetProperties(BindingFlags.Public);
+            foreach (var propertyInfo in objectProperties)
+            {
+                //need to tell classes and IEnumerables apart from the rest of the accepted data types.
+                if (_acceptedDataTypes.Contains(propertyInfo.PropertyType))
+                    serializedObject += "\"" + propertyInfo.Name + "\" : \"" + propertyInfo.GetValue(classobject) + "\",\n";
+                else if (propertyInfo is IEnumerable)
+                    ;
+            }
+            serializedObject += "}";
+            return serializedObject;
         }
     }
 }
