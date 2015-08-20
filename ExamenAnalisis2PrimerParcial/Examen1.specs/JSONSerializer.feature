7aecbe75a2a1e4e3df30eab7e67bffdd07ff9b76
Allan Mejia <mejia561@hotmail.com>
2015-08-19 23:21:03 -0600

    Installed SpecFlow

---
+++ ExamenAnalisis2PrimerParcial/Examen1.specs/JSONSerializer.feature
@@ -0,0 +1,10 @@
+﻿Feature: JSONSerializer
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Return Empty JSON
+	Given I have an empty Class
+	When I press serialize
+	Then the result should be {}
---
+++ ExamenAnalisis2PrimerParcial/Examen1.specs/JSONSerializerSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Examen1.specs
+{
+    [Binding]
+    public class JSONSerializerSteps
+    {
+        [Given(@"I have an empty Class")]
+        public void GivenIHaveAnEmptyClass()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press serialize")]
+        public void WhenIPressSerialize()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be \{}")]
+        public void ThenTheResultShouldBe()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
